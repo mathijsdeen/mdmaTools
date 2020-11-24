38dbd802f21202002375042a1d0e1b73fb79da19
Mathijs Deen <mathijsdeen@gmail.com>
2020-11-24 11:21:57 +0100

    repair parsing mishap in .Rd file, add example

--- R/nin.R
+++ R/nin.R
@@ -1,10 +1,12 @@
 #' @title \%nin\% function
-#' @description Evaluates of the left hand side argument is not in the right hand side argument. As such, it negates the `%in%` operator.
+#' @description Evaluates whether the left hand side argument is not in the right hand side argument. As such, it negates the \code{\%in\%} operator.
 #' @param lhs left hand side
 #' @param rhs right hand side
 #' @return The function returns
 #' \item{out}{a vector of the same length as \code{lhs}.}
 #' @details blabla
+#' @examples
+#' c(1,2,3) %nin% c(1,2)
 #' @author Mathijs Deen
 #' @export
 `%nin%` <- function(lhs, rhs){
