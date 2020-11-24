f828831aeb025b1ee904d670d1af5c92e7067149
Mathijs Deen <mathijsdeen@gmail.com>
2020-11-24 17:55:22 +0100

    adaptations to manual

--- R/nin.R
+++ R/nin.R
@@ -1,10 +1,9 @@
 #' @title \%nin\% function
-#' @description Evaluates whether the left hand side argument is not in the right hand side argument. As such, it negates the \code{\%in\%} operator.
+#' @description Evaluates whether the left hand side argument is not in the right hand side argument.
 #' @param lhs left hand side
 #' @param rhs right hand side
-#' @return The function returns
-#' \item{out}{a vector of the same length as \code{lhs}.}
-#' @details blabla
+#' @return The function returns a vector of the same length as \code{lhs}.
+#' @details The \code{\%nin\%} function negates the \code{\%in\%} operator.
 #' @examples
 #' c(1,2,3) %nin% c(1,2)
 #' @author Mathijs Deen
